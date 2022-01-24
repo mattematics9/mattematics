bf76328417f22d5db981dea9ca68587c16b5d003
Matthew Mannina <mattematics9@gmail.com>
2022-01-24 01:30:11 -0500

    basic setup

---
+++ dist/main.97a837676d4d98ea16d8.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hey');
+console.log('yoyo');
+console.log('yoyoo');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi45N2E4Mzc2NzZkNGQ5OGVhMTZkOC5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL21hdHRlbWF0aWNzLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZXknKTtcbmNvbnNvbGUubG9nKCd5b3lvJyk7XG5jb25zb2xlLmxvZygneW95b28nKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'MATTEMATICS',
+        filename: 'index.html',
+        template: path.resolve(__dirname, './src/template.html')
+    })],
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 5001,
+        open: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},  
+            {test: /\.(svg|ico|png|jpeg|jpg|gif|webp)$/, type: 'asset/resource'},
+            {   test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            }
+        ]
+    }
+}
