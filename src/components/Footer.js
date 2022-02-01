096168a17d3b4a48d50ab5dfaa9bc2436cedde0f
Matthew Mannina <mattematics9@gmail.com>
2022-02-01 16:08:57 -0500

    added firebase hosting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className='grey lighten-2 grey-text text-darken-3'>
             <div className='container center' style={{paddingTop: '50px', paddingBottom: '50px'}}>
                 <h6>MATTEMATICS WEB DEVELOPMENT</h6>
-                <h6>info@mattematics.com</h6>
+                <h6>mattematics9@gmail.com</h6>
                 <h6>631-672-3423</h6>
             </div>
         </div>
